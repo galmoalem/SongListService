855c222176604cfba47d327cc71d960161d23419
User <User@DESKTOP-H5EI584>
2020-05-26 11:42:13 +0300

    upload

---
+++ SongListsService/src/main/java/SongListsService/layout/SongListBoundry.java
@@ -0,0 +1,131 @@
+package SongListsService.layout ; 
+
+import java.time.LocalDate; 
+import java.time.format.DateTimeFormatter;
+
+import SongListsService.data.SongList;
+
+public class SongListBoundry {
+	private String id ;
+	private String name;
+	private String userEmail;
+	private String createdTimestamp;
+
+	public SongListBoundry() {
+
+	}
+	
+		
+	public String getId() {
+		return id;
+	}
+
+
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+
+
+
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+
+
+
+
+	public String getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+
+
+
+
+
+	public void setCreatedTimestamp(String createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+
+
+
+
+
+	public SongListBoundry(SongList songslist) {
+		if(songslist.getId() != null)
+{    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+     String formattedString = songslist.getCreatedTimestamp().format(formatter);
+			this.createdTimestamp=formattedString;
+}
+		if(songslist.getId() != null)
+		{
+			this.id=songslist.getId();
+		}
+		
+		if(songslist.getName() != null) {
+			this.name = songslist.getName();
+		}
+		if(songslist.getUserEmail() != null) {
+			this.userEmail = songslist.getUserEmail();
+		}
+	}
+	
+
+	public SongList convertToEntity () {
+		
+		SongList sList = new SongList();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		LocalDate dt;
+		
+		dt = LocalDate.parse(this.getCreatedTimestamp(),dtf);
+	    sList.setCreatedTimestamp(dt);
+	
+	    if (getUserEmail() != null) {
+	    	sList.setUserEmail(this.userEmail);
+	    }
+	    
+	    if(getId() != null ) {
+	    	sList.setId(this.id);
+	    }
+	
+	if(getName() != null) {
+		sList.setName(this.name);
+	}
+	
+	
+	return sList;
+	}
+	
+}
+
+
+
